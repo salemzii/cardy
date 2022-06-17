39dc2a977a78f08030e443a4f919fe9265a55380
salemzii <salemododa2@gmail.com>
2022-06-17 06:00:27 +0100

    --

--- service/middlewares.go
+++ service/middlewares.go
@@ -12,14 +12,14 @@ import (
 
 func SetContentTypeMiddleware(h http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    	data, err := ioutil.ReadFile("/home/salem/Desktop/cardy/data.csv")
+    	data, err := ioutil.ReadFile("data.csv")
 		if err != nil {
 			fmt.Println(err)
 		}
 
 		w.Header().Set("Content-Type", "application/octet-stream")
 		w.Header().Set("Content-Disposition", "attachment; filename=data.csv")
-		
+
 		io.Copy(w, bytes.NewReader(data))
         log.Println("middleware", r.URL)
         h.ServeHTTP(w, r)
